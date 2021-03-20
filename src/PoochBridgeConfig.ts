ed8bb6c1b3deecbfafd332967c35a8eee2624ac6
Michael Yankelev <myankelev@gmail.com>
2021-03-20 21:24:28 +0200

    new contract addressses

--- src/PoochBridgeConfig.ts
+++ src/PoochBridgeConfig.ts
@@ -46,13 +46,13 @@ export const chainbridgeConfig: ChainbridgeConfig = {
       nativeTokenSymbol: "ETH",
       tokens: [
         {
-          address: "0x4467982362af543Ab8EbC6BEE25D33CF8d13bA1A",
+          address: "0x6B50527ca7176da6cdD251613fCC0c4968135Fb2",
           name: "Genisis pooch",
           symbol: "Bork",
           imageUri: "",
           type: "ERC721",
           resourceId:
-            "0x0000000000000000000000004467982362af543Ab8EbC6BEE25D33CF8d13bA1A",
+            "0x0000000000000000000000006B50527ca7176da6cdD251613fCC0c4968135Fb2",
         },
       ],
     },
@@ -69,55 +69,15 @@ export const chainbridgeConfig: ChainbridgeConfig = {
       nativeTokenSymbol: "ETC",
       tokens: [
         {
-          address: "0x4467982362af543Ab8EbC6BEE25D33CF8d13bA1A",
+          address: "0x7A762587E1106a7CfF3F14aB44C9c383f9aB34Ea",
           name: "Genisis pooch",
           symbol: "Bork",
           imageUri: "",
           type: "ERC721",
           resourceId:
-            "0x0000000000000000000000004467982362af543Ab8EbC6BEE25D33CF8d13bA1A",
+            "0x0000000000000000000000006B50527ca7176da6cdD251613fCC0c4968135Fb2",
         },
       ],
     },
   ],
-
-  // DEVNET
-  //   erc20ResourceId:
-  //   "0x00000000000000000000000021605f71845f372A9ed84253d2D024B7B10999f4",
-  // chains: [
-  // {
-  //   chainId: 1,
-  //   networkId: 5,
-  //   name: "Ethereum - A",
-  //   bridgeAddress: "0x62877dDCd49aD22f5eDfc6ac108e9a4b5D2bD88B",
-  //   erc20HandlerAddress: "0x3167776db165D8eA0f51790CA2bbf44Db5105ADF",
-  //   rpcUrl: "http://localhost:8545",
-  //   type: "Ethereum",
-  //   tokens: [
-  //     {
-  //       address: "0x21605f71845f372A9ed84253d2D024B7B10999f4",
-  //       name: "Test EthA",
-  //       symbol: "TESTA",
-  //       imageUri: ETHIcon,
-  //     },
-  //   ],
-  // },
-  // {
-  //   chainId: 2,
-  //   networkId: 6,
-  //   name: "Ethereum - B",
-  //   bridgeAddress: "0x62877dDCd49aD22f5eDfc6ac108e9a4b5D2bD88B",
-  //   erc20HandlerAddress: "0x3167776db165D8eA0f51790CA2bbf44Db5105ADF",
-  //   rpcUrl: "http://localhost:8546",
-  //   type: "Ethereum",
-  //   tokens: [
-  //     {
-  //       address: "0x21605f71845f372A9ed84253d2D024B7B10999f4",
-  //       name: "Test EthB",
-  //       symbol: "TESTB",
-  //       imageUri: ETHIcon,
-  //     },
-  //   ],
-  // },
-  // ]
 };
