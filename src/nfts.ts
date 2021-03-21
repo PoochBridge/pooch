c14e80e599792537c6690c89f2fa65a03b9f2969
Michael Yankelev <myankelev@gmail.com>
2021-03-21 17:48:25 +0200

    update metadata

--- src/nfts.ts
+++ src/nfts.ts
@@ -18,7 +18,7 @@ const nfts: ERC721Metadata[] = [
     rarity: 1,
     lastPrice: 1.4,
     name: "Jasper",
-    id: "0x1",
+    id: "0x101",
   },
   {
     image: TwoSVG,
@@ -27,7 +27,7 @@ const nfts: ERC721Metadata[] = [
     rarity: 1,
     lastPrice: 2.65,
     name: "Ezra",
-    id: "0x2",
+    id: "0x102",
   },
   {
     image: ThreeSVG,
@@ -36,7 +36,7 @@ const nfts: ERC721Metadata[] = [
     rarity: 1,
     lastPrice: 5.2,
     name: "Wayne",
-    id: "0x3",
+    id: "0x103",
   },
   {
     image: FourSVG,
@@ -45,7 +45,7 @@ const nfts: ERC721Metadata[] = [
     rarity: 1,
     lastPrice: 7.5,
     name: "HUXLEY",
-    id: "0x4",
+    id: "0x104",
   },
   {
     image: FiveSVG,
@@ -54,7 +54,7 @@ const nfts: ERC721Metadata[] = [
     rarity: 1,
     lastPrice: 8.5,
     name: "KAI",
-    id: "0x5",
+    id: "0x105",
   },
   {
     image: SixSVG,
@@ -63,7 +63,7 @@ const nfts: ERC721Metadata[] = [
     rarity: 1,
     lastPrice: 10.4,
     name: "LORELEI",
-    id: "0x6",
+    id: "0x106",
   },
   {
     image: SevenSVG,
@@ -72,7 +72,7 @@ const nfts: ERC721Metadata[] = [
     rarity: 1,
     lastPrice: 12.4,
     name: "MILA",
-    id: "0x7",
+    id: "0x107",
   },
   {
     image: EightSVG,
@@ -81,7 +81,7 @@ const nfts: ERC721Metadata[] = [
     rarity: 1,
     lastPrice: 15.6,
     name: "Wayne",
-    id: "0x8",
+    id: "0x108",
   },
   {
     image: NineSVG,
@@ -90,17 +90,17 @@ const nfts: ERC721Metadata[] = [
     rarity: 1,
     lastPrice: 18.3,
     name: "ELLIE",
-    id: "0x9",
+    id: "0x109",
   },
-  {
-    image: TenSVG,
-    category: "Trading Cards",
-    collection: "OGPooch",
-    rarity: 1,
-    lastPrice: 19.2,
-    name: "MINNIE",
-    id: "0x10",
-  },
+  // {
+  //   image: TenSVG,
+  //   category: "Trading Cards",
+  //   collection: "OGPooch",
+  //   rarity: 1,
+  //   lastPrice: 19.2,
+  //   name: "MINNIE",
+  //   id: "0x1010",
+  // },
 ];
 
 export const getNftMetadata = (id: string): ERC721Metadata | undefined =>
